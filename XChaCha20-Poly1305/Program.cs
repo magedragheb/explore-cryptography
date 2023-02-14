a4668dc57db6ee77ac873e9d07d81c466f2fd7ba
Maged <19859654+magedragheb@users.noreply.github.com>
2023-02-14 12:07:26 +0200

    implement XChaCha20Poly1305

---
+++ XChaCha20-Poly1305/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography;
+using NSec.Cryptography;
+using System.Text;
+
+var Algorithm = AeadAlgorithm.XChaCha20Poly1305;
+
+//https://nsec.rocks/docs/api/nsec.cryptography.keyexportpolicies
+//default policy is none, which protects the key from reading
+var param = new KeyCreationParameters
+{ ExportPolicy = KeyExportPolicies.AllowPlaintextArchiving };
+
+using var key = new Key(Algorithm, param);
+
+Console.WriteLine($"Key: {Convert.ToBase64String(key.Export(KeyBlobFormat.NSecSymmetricKey))}");
+
+var nonce = new byte[Algorithm.NonceSize];
+RandomNumberGenerator.Fill(nonce);
+Console.WriteLine($"Nonce: {Convert.ToBase64String(nonce)}");
+
+var tag = new byte[Algorithm.TagSize];
+RandomNumberGenerator.Fill(tag);
+Console.WriteLine($"Tag: {Convert.ToBase64String(tag)}");
+
+var message = Encoding.UTF8.GetBytes("sample text to encrypt with XChaCha20Poly1305 with key length 256!");
+var cipher = XChaCha20Poly1305.XChaCha20Poly1305.Encrypt(key, nonce, tag, message);
+var decoded = XChaCha20Poly1305.XChaCha20Poly1305.Decrypt(key, nonce, tag, cipher);
+
+Console.WriteLine($"Cipher: {Convert.ToBase64String(cipher)}");
+Console.WriteLine(decoded != null ? $"Decoded: {Encoding.UTF8.GetString(decoded)}" : "-");
---
+++ XChaCha20-Poly1305/readme.md
@@ -0,0 +1,3 @@
+Original article: https://www.scottbrady91.com/c-sharp/xchacha20-poly1305-dotnet
+
+I am using [NSec](https://github.com/ektrah/nsec) which is also based on libsodium
